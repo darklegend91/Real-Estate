a097ebfdfa856bcc42fd8a98dcfb26071ffe68cd
Aditya Pathania <78314428+darklegend91@users.noreply.github.com>
2024-09-02 19:50:30 +0530

    Update products-desc.js

--- js/products-desc.js
+++ js/products-desc.js
@@ -12,7 +12,7 @@ function img1() {
     let imageElement = document.getElementById("img-2");
     imageElement.src = "../kitchen.jpg";
   }
-  function img4() {
-    let imageElement = document.getElementById("img-2");
-    imageElement.src = "../workspace.jpg";
-  }
+  // function img4() {
+  //   let imageElement = document.getElementById("img-2");
+  //   imageElement.src = "../workspace.jpg";
+  // }
