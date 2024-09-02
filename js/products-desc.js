7e72a5e3828dda891862194373e990f835c153ed
Aditya Pathania <78314428+darklegend91@users.noreply.github.com>
2024-09-02 14:26:37 +0530

    Files uploaded.

---
+++ js/products-desc.js
@@ -0,0 +1,18 @@
+function img1() {
+    let imageElement = document.getElementById("img-2");
+    imageElement.src = "../assets/bedroom.jpg";
+  }
+
+  function img2() {
+    let imageElement = document.getElementById("img-2");
+    imageElement.src = "../assets/workspace.jpg";
+  }
+
+  function img3() {
+    let imageElement = document.getElementById("img-2");
+    imageElement.src = "../assets/kitchen.jpg";
+  }
+  function img4() {
+    let imageElement = document.getElementById("img-2");
+    imageElement.src = "../assets/kitchen.jpg";
+  }
